c105fe54dee66840e205344e04f51574b5451831
cookieseventeen <cookie_seventeen@msn.com>
2024-04-22 23:45:35 +0800

    新增等價寫法

--- index.ts
+++ index.ts
@@ -14,12 +14,22 @@ const odd= oddNum=>{
   return oddNum%2 === 1 ? oddNum:0;
 }
 
+const even= anyNum=>anyNum%2===0?anyNum:0;//等價寫法
+
 
 const oddSum=anySum(odd);
+
+const evenSum=anySum(even);
+const evenSumTwo=anySum(anyNum=>anyNum%2===0?anyNum:0);//等價寫法
+
 
 let anyArray=[1,2,3,4,5,6,7,8,9];
 
 
 const oddTotal=oddSum(anyArray);
+const evenTotal=evenSum(anyArray);
+const evenTotalTwo=evenSumTwo(anyArray);
 
 console.log(oddTotal);
+console.log(evenTotal);
+console.log(evenTotalTwo);
